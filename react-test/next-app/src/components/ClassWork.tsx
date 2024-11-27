a9e33a238f173d5f2f17cf918923eeb9b749b33e
sojiro takagi <soujirou.takagi@gmail.com>
2024-11-28 00:51:40 +0900

    APIの取得

--- react-test/next-app/src/components/ClassWork.tsx
+++ react-test/next-app/src/components/ClassWork.tsx
@@ -1,13 +1,35 @@
+import { useEffect } from "react";
+
 type Props = {
     selectedClassroom: number;
     selectedDayIndex: number;
 };
 
 export default function ClassWork(props: Props) {
+    useEffect(() => {
+        const fetchClassWork = async () => {
+            try {
+                const res = await fetch(
+                    `https://click.ecc.ac.jp/ecc/webdesign/wdct/api/v1/openrooms/timetables/?classroom=${props.selectedClassroom}&weekday=${props.selectedDayIndex}`,
+                    { cache: "no-store" }
+                );
+                if (!res.ok) {
+                    throw new Error("データの取得に失敗しました");
+                }
+                const data = await res.json();
+                console.log("取得したデータ:", data);
+            } catch (error) {
+                console.error("エラー:", error);
+            }
+        };
+        fetchClassWork();
+    }, [props.selectedClassroom, props.selectedDayIndex]);
+
     return (
         <div>
-            {props.selectedClassroom}
-            {props.selectedDayIndex}
+            教室{props.selectedClassroom}
+            <br />
+            曜日{props.selectedDayIndex}
         </div>
     );
 }
